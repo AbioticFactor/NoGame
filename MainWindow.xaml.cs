2e7ba23f06c3099a9460b63f401c5f3a37f276ab
Tobi Alade <aoalade390@gmail.com>
2019-12-29 20:43:55 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using MingweiSamuel.Camille;
+using MingweiSamuel.Camille.SummonerV4;
+using MingweiSamuel.Camille.SpectatorV4;
+using MingweiSamuel.Camille.Enums;
+
+namespace NoGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer;
+        TimeSpan time;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        String path;
+        String summoner;
+        double hours;
+        String key = "RGAPI-1a15a384-ba51-464a-9a31-3c1f5a526ed2";
+
+        void OpenSettings(object sender, RoutedEventArgs e)
+        {
+            Settings set = new Settings();
+            set.Owner = this;
+
+            if (set.ShowDialog() == true)
+            {
+                path = set.Path;
+                hours = set.Hours;
+                summoner = set.Summoner;
+            }
+        }
+
+
+        void Play(object sender, RoutedEventArgs e)
+        {
+
+            // Run game
+            Process.Start(path);
+
+            var riotApi = RiotApi.NewInstance(key);
+            var player = riotApi.SummonerV4.GetBySummonerName(Region.NA, summoner);
+            var game = riotApi.SpectatorV4.GetCurrentGameInfoBySummoner(Region.NA, player.Id);
+
+
+            time = TimeSpan.FromSeconds(30);
+
+            timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+            {
+                lblTime.Content = time.ToString("c");
+
+                if ((time == TimeSpan.Zero) && (game == null))
+                {
+                    timer.Stop();
+
+
+                    KillLeague();
+                }
+                time = time.Add(TimeSpan.FromSeconds(-1));
+            }, Application.Current.Dispatcher);
+            timer.Start();
+
+
+        }
+
+        public static void KillLeague()
+        {
+            System.Diagnostics.Process[] procs = null;
+            try
+            {
+                procs = Process.GetProcessesByName("LeagueClientUx");
+
+                Process LeagueClientUxProc = procs[0]; 
+
+                if (!LeagueClientUxProc.HasExited)
+                {
+                    LeagueClientUxProc.Kill();
+                }
+            }
+            finally
+            {
+                if (procs != null)
+                {
+                    foreach (Process p in procs)
+                        p.Dispose();
+                }
+
+            }
+
+
+
+
+        }
+    }
+}
+
---
+++ Settings.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+namespace NoGame
+{
+    /// <summary>
+    /// Interaction logic for Settings.xaml
+    /// </summary>
+    public partial class Settings : Window
+    {
+        public Settings()
+        {
+            InitializeComponent();
+
+
+        }
+
+        void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            // Dialog box canceled
+            this.DialogResult = false;
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                gamepath.Text = System.IO.Path.GetFullPath(openFileDialog.FileName);
+        }
+
+        public string Path
+        {
+            get { return gamepath.Text; }
+        }
+
+        public string Summoner
+        {
+            get { return summoner.Text; }
+        }
+
+        public int Hours
+        {
+            get { return int.Parse(hours.Text); }
+        }
+    }
+}
